e7da1205a10bd470cdc30a52502bb73c438c0088
Loïc STEINMETZ <loicsteinmetz22@gmail.com>
2021-01-20 19:05:18 +0100

    Refac

--- src/test/java/process/data/TestIcalMappingProcess.java
+++ src/test/java/process/data/TestIcalMappingProcess.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.TimeZone;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static utils.DateUtils.dateToString;

7f9fc09b38b596d55efead9bb82d50113c5f68fc
Loïc STEINMETZ <loicsteinmetz22@gmail.com>
2021-01-20 21:35:49 +0100

    refac

--- src/main/java/process/Publication.java
+++ src/main/java/process/Publication.java
@@ -3,38 +3,53 @@ package process;
 import models.Server;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
+import org.apache.logging.log4j.Logger;
+import utils.LoggerUtils;
 
 import javax.security.auth.login.LoginException;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 public abstract class Publication {
 
-    /**
-     *
-     * @param jda instance de connexion au serveur Discord
-     * @return true si le channel existe sur le server, sinon false
-     */
-    protected boolean hasChannel(JDA jda){
-        return !jda.getTextChannelsByName("emplois-du-temps", true).isEmpty();
-    }
+  private static final Logger LOGGER = LoggerUtils.buildLogger(Publication.class);
 
-    /**
-     *
-     * @param message à poster sur le server
-     * @param server qui reçoit le message
-     * @throws LoginException
-     * @throws InterruptedException
-     */
-    protected void sendMessage(String message, Server server) throws LoginException, InterruptedException {
-        JDA jda = JDABuilder.createDefault(server.getReference()).build();
-        jda.awaitReady();
-        if(hasChannel(jda)) {
-            TextChannel textChannel = jda.getTextChannelsByName("emplois-du-temps", true).get(0);
-            if (textChannel != null) {
-                textChannel.sendMessage(message).queue();
-            }
+  /**
+   * Envoie un message sur l'un des channel d'un serveur Discord.
+   *
+   * @param message à poster sur le server
+   * @param server  qui reçoit le message
+   * @throws LoginException       erreur de connexion au serveur discord
+   * @throws InterruptedException connexion rompue
+   */
+  protected boolean sendMessage(String message, Server server, String channel) throws LoginException, InterruptedException {
+    Message result = null;
+    JDA jda = JDABuilder.createDefault(server.getReference()).build();
+    jda.awaitReady();
+    if (hasChannel(jda, channel)) {
+      TextChannel textChannel = jda.getTextChannelsByName(channel, true).get(0);
+      try {
+        if (isNull(textChannel)) {
+          LOGGER.warn(
+            "Erreur lors de l'envoi d'un message dans un channel. Serveur : {}, Channel : {}",
+            server.getReference(), channel);
         }
+        result = textChannel.sendMessage(message).complete();
+      } catch (RuntimeException e) {
+        LOGGER.warn(
+          "Erreur lors de l'envoi d'un message. Serveur : {}",
+          server.getReference());
+      }
     }
+    return nonNull(result);
+  }
+
+  private boolean hasChannel(JDA jda, String channel) {
+    return !jda.getTextChannelsByName(channel, true).isEmpty();
+  }
 
 
 }
--- src/main/java/process/ScheduleUpdatePublicationProcess.java
+++ src/main/java/process/ScheduleUpdatePublicationProcess.java
@@ -1,38 +1,75 @@
 package process;
 
-
 import models.Server;
 import models.Session;
+import models.businessmodels.SessionChange;
 
 import javax.security.auth.login.LoginException;
-import java.text.SimpleDateFormat;
+import java.util.List;
 
+import static java.util.Objects.nonNull;
+import static utils.DateUtils.dateToString;
+import static utils.DateUtils.timeToString;
 
-public class ScheduleUpdatePublicationProcess extends Publication{
+/**
+ * Process de notification des mises à jour de l'emploi du temps.
+ */
+public class ScheduleUpdatePublicationProcess extends Publication {
 
-    /**
-     *
-     * @param oldSession, session qui est updated
-     * @param newSession, nouvelle session si le cours est reporté, si null est supprimé
-     * @throws LoginException
-     * @throws InterruptedException
-     */
-    public void sendPublication(Session oldSession, Session newSession) throws LoginException, InterruptedException {
-        String message = "@everyone \nChangement d'emploi du temps :information_source:```";
-        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
-        SimpleDateFormat sdf2 = new SimpleDateFormat("EEEE dd MMMMM");
-        message += "Cours du " + sdf2.format(oldSession.getDate()) + " de " + oldSession.getName() + " (" + sdf.format(oldSession.getStart()) + " - " + sdf.format(oldSession.getEnd()) + ") avec " + oldSession.getTeacher();
-        if(newSession != null){
-            message += " est reporté au " + sdf2.format(newSession.getDate()) + " (" + sdf.format(newSession.getStart()) + " - " + sdf.format(newSession.getEnd()) + ")";
-        }else{
-            message += " est supprimé";
-        }
-        message += "```";
-        for(Server s: oldSession.getSchedule().getServers()) {
-            sendMessage(message,s);
-        }
+  private static final String CHANNEL = "emploi-du-temps";
+
+  /**
+   * Envoie une publication listant les mises à jour de l'emploi du temps
+   * sur le serveur Discord concerné.
+   *
+   * @param changes modifications de l'emploi du temps
+   * @param server  serveur Discord
+   * @throws LoginException       erreur de connexion au serveur Discord
+   * @throws InterruptedException connexion interrompue
+   */
+  public void sendPublication(List<SessionChange> changes, Server server) throws LoginException, InterruptedException {
+    StringBuilder message = new StringBuilder("@everyone \nChangement d'emploi du temps :information_source:\n```\n");
+    fillMessage(changes, message);
+    message.append("\n```");
+    sendMessage(message.toString(), server, CHANNEL);
+  }
+
+  private void fillMessage(List<SessionChange> changes, StringBuilder message) {
+    for (SessionChange c : changes) {
+      fillMessageWithNewSession(message, c);
+      fillMessageWithReplacedSessions(message, c);
     }
+  }
 
+  private void fillMessageWithReplacedSessions(StringBuilder message, SessionChange c) {
+    if (!c.getReplacedSessions().isEmpty()) {
+      message.append("\n> Cours supprimés/modifiés :");
+      for (Session s : c.getReplacedSessions()) {
+        message
+          .append("\n    - ")
+          .append(s.getName())
+          .append(" (")
+          .append(timeToString(s.getStart()))
+          .append(" - ")
+          .append(timeToString(s.getEnd()))
+          .append(")");
+      }
+    }
+  }
 
-
+  private void fillMessageWithNewSession(StringBuilder message, SessionChange c) {
+    Session ns = c.getNewSession();
+    message.append(c.getReplacedSessions().isEmpty() ? "\nNOUVEAU COURS :\n" : "\nMODIFICATION :\n");
+    message
+      .append(c.getNewSession().getName().toUpperCase())
+      .append(" - le ").append(dateToString(ns.getDate()))
+      .append(" de ").append(timeToString(ns.getStart()))
+      .append(" à ").append(timeToString(ns.getStart()));
+    if (nonNull(ns.getTeacher())) {
+      message.append(" (").append(ns.getTeacher()).append(")");
+    }
+    if (nonNull(ns.getLocation())) {
+      message.append(" - ").append(ns.getLocation());
+    }
+  }
 }

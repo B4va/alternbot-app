26e4174e496d900f11962cdd3fc722ca7c4c24ee
Loïc STEINMETZ <loicsteinmetz22@gmail.com>
2021-01-23 23:33:40 +0100

    fix

--- src/main/java/controllers/workers/schedule/ScheduleWorkersHandler.java
+++ src/main/java/controllers/workers/schedule/ScheduleWorkersHandler.java
@@ -2,8 +2,6 @@ package controllers.workers.schedule;
 
 import controllers.workers.WorkersHandler;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * Gère les opérations automatiques relative à l'emploi du temps.
  */
@@ -11,7 +9,7 @@ public class ScheduleWorkersHandler extends WorkersHandler {
 
   @Override
   public WorkersHandler init() {
-    runnables.add(new SchedulesUpdateWorker(TimeUnit.HOURS.toMillis(1), 0));
+    // TODO
     return this;
   }
 }

f5cbb087beb4a8c1b1d7dda487a9af42f1d68d15
Loïc STEINMETZ <loic.steinmetz2@gmail.com>
2021-02-07 19:19:02 +0100

    doc

--- src/main/java/process/data/TaskDeletionProcess.java
+++ src/main/java/process/data/TaskDeletionProcess.java
@@ -6,15 +6,24 @@ import models.Model;
 import models.Server;
 import models.Task;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.User;
-
-import javax.security.sasl.AuthenticationException;
-import java.security.AccessControlException;
 
 import static java.util.Objects.nonNull;
 
+/**
+ * Process de suppression d'une tâche par un utilisateur autorisé.
+ */
 public class TaskDeletionProcess extends TaskAccessor {
 
+  /**
+   * Supprime une tâche si l'utilisateur y est autorisé.
+   *
+   * @param taskId identifiant de la tâche à supprimer
+   * @param server serveur à partir duquel est émise la demande de suppression
+   * @param member utilisateur ayant initié l'opération
+   * @return true si l'opération a pu être réalisée
+   * @throws ServerAccessException la tâche n'est pas associée au serveur
+   * @throws MemberAccessException l'utilisateur n'est pas autorisé à modifier les tâches
+   */
   public boolean delete(int taskId, Server server, Member member) throws ServerAccessException, MemberAccessException {
     Task task = Model.read(taskId, Task.class);
     if (nonNull(task)) {

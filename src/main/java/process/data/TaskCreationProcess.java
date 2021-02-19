f1a7d9c949dd1c6e7181f8655e5f0b0e1b69d5cf
Alexis <metin547100@gmail.com>
2021-02-19 17:38:07 +0100

    Update TaskCreationProcess.java

--- src/main/java/process/data/TaskCreationProcess.java
+++ src/main/java/process/data/TaskCreationProcess.java
@@ -1,5 +1,6 @@
 package process.data;
 
+import com.sun.media.sound.InvalidDataException;
 import exceptions.MemberAccessException;
 import exceptions.ServerAccessException;
 import models.Server;
@@ -18,13 +19,17 @@ public class TaskCreationProcess extends TaskAccessor{
     /**
      * Création d'une tâche si l'utilisateur y est autorisé.
      *
-     * @param task tâche à créer
+     * @param description de la tâche
+     * @param dueDate date de rendu de la tâche(jour)
+     * @param dueTime date de rendu de la tâche(heure)
      * @param member utilisateur ayant initié l'opération
      * @param server serveur à partir duquel est émise la demande de création
      * @return true si l'opération a pu être réalisée
      * @throws MemberAccessException l'utilisateur n'est pas autorisé à créer les tâches
      */
-    public boolean create(Task task,Member member, Server server) throws ServerAccessException, MemberAccessException {
+    public boolean create(String description,Date dueDate,Date dueTime,Member member, Server server) throws ServerAccessException, MemberAccessException, InvalidDataException {
+        Task task = validateTask(description,dueDate,dueTime);
+        task.setServer(server);
         if (nonNull(task)) {
             if (isMemberAuthorized(member)) {
                 task.create();
@@ -34,5 +39,21 @@ public class TaskCreationProcess extends TaskAccessor{
             }
         }
         return false;
+    }
+
+    /**
+     * Vérification de la validité des attributs de la tâche
+     * @param description de la tâche
+     * @param dueDate date de rendu de la tâche(jour)
+     * @param dueTime date de rendu de la tâche(heure)
+     * @return Task l'instance de la tâche si elle est valide
+     * @throws InvalidDataException
+     */
+    private Task validateTask(String description,Date dueDate,Date dueTime) throws InvalidDataException{
+        if(description == null || dueDate == null || dueTime == null){
+            throw new InvalidDataException();
+        }else{
+            return new Task(description,dueDate,dueTime,null);
+        }
     }
 }

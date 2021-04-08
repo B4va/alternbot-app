6b6804c06f227394bab1acc96b2b491a42c7443e
Loïc STEINMETZ <loicsteinmetz22@gmail.com>
2021-04-08 11:14:57 +0200

    [3] FIX - Erreur en cas de mauvais format de l'horaire d'une tâche

--- src/test/java/controllers/task/commands/TestTaskOperationsCommandListener.java
+++ src/test/java/controllers/task/commands/TestTaskOperationsCommandListener.java
@@ -1,14 +1,13 @@
 package controllers.task.commands;
 
-import controllers.task.commands.TaskOperationsCommandListener;
 import org.junit.jupiter.api.Test;
 
+import java.time.DateTimeException;
 import java.util.Arrays;
 import java.util.Map;
 
 import static controllers.task.commands.TaskOperationsCommandListener.*;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Classe de test de {@link TaskOperationsCommandListener}
@@ -34,6 +33,20 @@ public class TestTaskOperationsCommandListener {
       () -> assertEquals(map.get(MAP_DESCRIPTION), "test de création"),
       () -> assertEquals(map.get(MAP_DUE_DATE), "01-01-2020"),
       () -> assertEquals(map.get(MAP_DUE_TIME), "10:00")
+    );
+  }
+
+  @Test
+  public void testParseMessage_error_time_format() {
+    String message1 = "$tache -u 1 [ test de création ] 01-01-2020 -1:00";
+    String message2 = "$tache -u 1 [ test de création ] 01-01-2020 26:00";
+    String message3 = "$tache -u 1 [ test de création ] 01-01-2020 10:-5";
+    String message4 = "$tache -u 1 [ test de création ] 01-01-2020 10:77";
+    assertAll(
+      () -> assertThrows(DateTimeException.class, () -> parseMessage(Arrays.asList(message1.split(" ")))),
+      () -> assertThrows(DateTimeException.class, () -> parseMessage(Arrays.asList(message2.split(" ")))),
+      () -> assertThrows(DateTimeException.class, () -> parseMessage(Arrays.asList(message3.split(" ")))),
+      () -> assertThrows(DateTimeException.class, () -> parseMessage(Arrays.asList(message4.split(" "))))
     );
   }
 }
